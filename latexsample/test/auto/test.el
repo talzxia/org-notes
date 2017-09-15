3430db07b401b2ab4b94da5f32404ed713f8ceb1
talzxia <admin@example.com>
2017-09-15 21:46:58 +0800

    2017-9-15
    21：46

--- latexsample/test/auto/test.el
+++ latexsample/test/auto/test.el
@@ -1,10 +1,11 @@
 (TeX-add-style-hook
  "test"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("ctexbook" "12pt" "a4paper")))
    (TeX-run-style-hooks
     "latex2e"
     "ctexbook"
-    "ctexbook10"
-    "xeCJK"))
+    "ctexbook12"))
  :latex)
 
