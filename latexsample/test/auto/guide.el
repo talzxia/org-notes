375515804bd472f118a70328be387dc618ae423e
talzxia <admin@example.com>
2017-09-16 22:00:14 +0800

    2017-9-16
    22:00

---
+++ latexsample/test/auto/guide.el
@@ -0,0 +1,49 @@
+(TeX-add-style-hook
+ "guide"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("elegantbook" "color=green" "mathpazo" "titlestyle=hang" "green" "hang" "color=X" "Y" "titlestyle=Z")))
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
+   (TeX-run-style-hooks
+    "latex2e"
+    "elegantbook"
+    "elegantbook10"
+    "makecell"
+    "lipsum"
+    "texnames"
+    "fancyvrb")
+   (LaTeX-add-labels
+    "figur:happy"
+    "tab:color thm")
+   (LaTeX-add-bibliographies
+    "reference")
+   (LaTeX-add-xcolor-definecolors
+    "main1"
+    "seco1"
+    "thid1"
+    "main2"
+    "seco2"
+    "thid2"
+    "main3"
+    "seco3"
+    "thid3"
+    "main"
+    "seco"
+    "thid"))
+ :latex)
+
--- latexsample/test/auto/ziju.el
+++ latexsample/test/auto/ziju.el
@@ -1,11 +1,12 @@
 (TeX-add-style-hook
- "test"
+ "ziju"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("ctexbook" "12pt" "a4paper")))
    (TeX-run-style-hooks
     "latex2e"
     "ctexbook"
-    "ctexbook12"))
+    "ctexbook12"
+    "geometry"))
  :latex)
 
