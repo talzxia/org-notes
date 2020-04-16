79a7924d303993f45ec5832b1f1871628832eb48
talzxia <talzxia@126.com>
2020-04-16 09:58:31 +0800

    4-16
    10:00

---
+++ C-learning/21firt.c
@@ -0,0 +1,17 @@
+//
+//2.1first.c
+// first
+//
+//Created by Xia on 2020/04/16
+//Copyright @ 2020 Xia All rights reserved.
+//
+#include<stdio.h>
+int main(int argc,const char *argv[]) /*一个简单的C程序*/
+{
+    int num;  /*定义一个名为num的变量*/
+    num=1;  /*为num赋一个值*/
+    printf("I am a simple ");  /*使用printf()函数*/
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n",num);
+    return 0;
+}
