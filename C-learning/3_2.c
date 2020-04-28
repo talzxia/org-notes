91d615a88284ce45a7acc8e0148ed50661d564ba
talzxia <talzxia@126.com>
2020-04-29 07:14:35 +0800

    7:15

--- C-learning/3_2.c
+++ C-learning/3_2.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     int ten=10;
     int two=2;
     printf("Doing it right:");
-    printf("%d minus %d is %d\n",ten,two,ten-two);
+    printf("%d minus %d is %d\n",ten,2,ten-two);
     printf("Doing it wrong:");
     printf("%d minus %d is %d\n",ten);//遗漏两个参数
     return 0;
