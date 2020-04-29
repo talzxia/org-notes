d5c43af558af50fe52e933e1c7718dbe17f2ea68
talzxia <talzxia@126.com>
2020-04-29 15:53:04 +0800

    2020-4-29 15:54

---
+++ C-learning/bases_3_3.c
@@ -0,0 +1,15 @@
+//
+// 3.3程序清单bases。c
+//bases_3_3.c
+//
+//Created by Xia on 2020/04/29
+//Copyright @ 2020 Xia All Rights Reserved
+//
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+    int x=100;
+    printf("dec=%d; octal=%o; hex=%x\n",x,x,x);
+    printf("dec=%d; octal=%#o; hex=%#x\n",x,x,xa)
+    return 0;
+}
