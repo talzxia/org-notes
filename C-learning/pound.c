bb5885301b96d783a5192ca9218c4515f6e218d6
talzxia <talzxia@126.com>
2020-08-30 11:45:20 +0800

    11:45

---
+++ C-learning/pound.c
@@ -0,0 +1,20 @@
+//pound.c --定义一个带一个参数的函数
+#include<stdio.h>
+void pound(int n);  //ANSI函数原型声明
+int main(void)
+{
+    int times=5;
+    char ch='!';  //ASCII码是33
+    float f=6.0f;
+    pound(times);  //int类型的参数
+    pound(ch);  //和pound（（int）ch}；相同
+    pound(f);  //和 pound((int)f);相同
+    return 0;
+}
+void pound(int n)  //ANSI风格函数头
+{  //表明该函数接受一个int类型的参数
+    while(n-->0)
+        printf("#");
+    printf("\n");
+    
+}
